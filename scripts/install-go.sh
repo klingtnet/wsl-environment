a3bb6209f8169ab715a421aa31bdc296e8d2dbee
Andreas Linz <alinz@fastmail.com>
2020-04-03 12:42:16 +0200

    Install/Update go-tools after installing the Go distribution

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -6,3 +6,6 @@ _version="$1"
 cat <<HEREDOC | sudo sh
 curl -Lsf 'https://dl.google.com/go/go${_version}.linux-amd64.tar.gz' | tar -C /usr/local -xzvf-
 HEREDOC
+
+# update/install go tools, e.g. goimports
+go get -u golang.org/x/tools/...
